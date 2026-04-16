f1a73a77e27b8c2d3177ff2a7ef6ea03ce82b692
Luka Meskhi <meskhiluk@gmail.com>
2026-04-16 20:11:13 +0400

    UI
    Update UI

--- my-app/src/components/atoms/Location/Location.jsx
+++ my-app/src/components/atoms/Location/Location.jsx
@@ -4,11 +4,11 @@ import Link from "next/link";
 export function LocationV1() {
      return(
          <div className='flex gap-0.5 items-center'>
-             <div className='flex gap-1 px-1 py-0.5'>
+             <div className='flex gap-1 px-1 items-center py-0.5'>
                  <Link href={'/'} className='text-[#666666] font-["Inter"] font-medium text-[18px] leading-[100%] tracking-normal'>Home</Link>
                  <Image src={'/rightArrow.svg'} alt={'arrowRight'} height={24} width={12} />
              </div>
-             <div className='px-1 py-0.5 flex items-center'>
+             <div className='px-1 py-0.5 items-center flex items-center'>
                  <p className='text-[#736BEA] font-["Inter"] font-medium text-[18px] leading-[100%] tracking-normal'>Browse</p>
              </div>
          </div>
@@ -18,11 +18,11 @@ export function LocationV1() {
 export function LocationV2() {
     return(
         <div className='flex gap-0.5 items-center'>
-            <div className='flex gap-1 px-1 py-0.5'>
+            <div className='flex gap-1 px-1 items-center py-0.5'>
                 <Link href={'/'} className='text-[#666666] font-["Inter"] font-medium text-[18px] leading-[100%] tracking-normal'>Home</Link>
                 <Image src={'/rightArrow.svg'} alt={'arrowRight'} height={24} width={12} />
             </div>
-            <div className='px-1 py-0.5 flex items-center'>
+            <div className='px-1 py-0.5 gap-1 flex items-center'>
                 <Link href={'/browse'} className='text-[#666666] font-["Inter"] font-medium text-[18px] leading-[100%] tracking-normal'>Browse</Link>
                 <Image src={'/rightArrow.svg'} alt={'arrowRight'} height={24} width={12} />
             </div>
--- my-app/src/components/molecules/BrowseCard/BrowseCard.jsx
+++ my-app/src/components/molecules/BrowseCard/BrowseCard.jsx
@@ -6,7 +6,8 @@ import Button from "@/components/atoms/Button/Button";
 const BrowseCard = ({ course }) => {
     return (
         <Link href={`/courses/${course.id}`} className="block">
-            <div className='flex flex-col gap-[18px] w-[373px] ransition-all duration-300 ease-out hover:border-[#B7B3F4] hover:shadow-[0px_0px_25px_0px_#8A82D41A] active:border-[#958FEF] active:shadow-[0px_0px_45px_0px_#8A82D426] bg-white rounded-[12px] p-[20px] border border-[#F5F5F5] cursor-pointer '>
+            <div className='flex flex-col gap-[18px] justify-between w-[373px] h-[451px] transition-all duration-300 ease-out hover:border-[#B7B3F4] hover:shadow-[0px_0px_25px_0px_#8A82D41A] active:border-[#958FEF] active:shadow-[0px_0px_45px_0px_#8A82D426] bg-white rounded-[12px] p-[20px] border border-[#F5F5F5] cursor-pointer '>
+                <div className='flex flex-col gap-[18px]'>
                 <Image
                     src={course.image || '/StartBGTry.svg'}
                     alt={course.title}
@@ -14,7 +15,7 @@ const BrowseCard = ({ course }) => {
                     height={181}
                     className="rounded-[8px] object-cover w-full h-[181px]"
                 />
-                <div className='flex flex-col gap-[16px]'>
+                <div className='flex flex-col  w-full gap-[16px]'>
                     <div className='flex justify-between items-center'>
                         <div className='flex gap-2'>
                             <span className='text-[#8A8A8A] font-inter font-medium text-[14px] leading-none tracking-normal'>
@@ -44,18 +45,19 @@ const BrowseCard = ({ course }) => {
                         </Button>
                     </div>
 
-                    <div className='flex justify-between items-center'>
-                        <div className='flex flex-col gap-1'>
+                </div>
+                </div>
+                <div className='flex justify-between items-center'>
+                    <div className='flex flex-col gap-1'>
                             <span className='font-inter font-medium text-[12px] leading-[100%] tracking-normal text-[#8A8A8A]'>
                                 Starting from
                             </span>
-                            <span className='text-[#292929] font-inter font-semibold text-2xl leading-none tracking-normal'>
+                        <span className='text-[#292929] font-inter font-semibold text-2xl leading-none tracking-normal'>
                                 ${parseFloat(course.basePrice).toFixed(0)}
                             </span>
-                        </div>
-                        <div className='font-inter font-medium text-[16px] leading-[24px] tracking-normal text-center text-white bg-[#4F46E5] rounded-[8px] px-[27px] py-[12px]'>
-                            Details
-                        </div>
+                    </div>
+                    <div className='font-inter font-medium text-[16px] leading-[24px] tracking-normal text-center text-white bg-[#4F46E5] rounded-[8px] px-[27px] py-[12px]'>
+                        Details
                     </div>
                 </div>
             </div>
